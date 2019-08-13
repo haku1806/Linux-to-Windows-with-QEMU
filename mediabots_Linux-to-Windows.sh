7a23dc4b373f2c35f930bb704ceb1836d34596bd
haku1806 <50793831+haku1806@users.noreply.github.com>
2019-08-14 03:19:49 +0700

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -232,6 +232,6 @@ fi
 #
 # Running the KVM
 echo "[ Running the KVM ]"
-if [ $skipped = 0 ] ; then
+#if [ $skipped = 0 ] ; then
 echo "[.] running QEMU-KVM"
 sudo $qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format -drive file=$custom_param_os,index=1,media=cdrom -drive file=$custom_param_sw,index=2,media=cdrom $other_drives -boot once=d -vnc :5 &	
