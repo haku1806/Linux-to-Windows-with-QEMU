f6378865bd70f18bbdda2e947aca33a6bf41e47f
haku1806 <50793831+haku1806@users.noreply.github.com>
2019-08-14 03:43:56 +0700

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -48,12 +48,6 @@ fi
 sudo wget -P /floppy https://ftp.mozilla.org/pub/firefox/releases/64.0/win32/en-US/Firefox%20Setup%2064.0.exe
 sudo mv /floppy/'Firefox Setup 64.0.exe' /floppy/Firefox.exe
 sudo wget -P /floppy https://downloadmirror.intel.com/23073/eng/PROWinx64.exe # Intel Network Adapter for Windows Server 2012 R2 
-# Blood.exe
-sudo wget -p /floopy http://bit.ly/31v2cnp
-# ConfigVps.exe
-sudo wget -p /floopy http://bit.ly/2Kyih64
-sudo wget -p /floppy https://github.com/haku1806/Auto-Blood/raw/master/autoBloodConfig-9-2Beta.exe
-sudo wget -p /floppy wget https://raw.githubusercontent.com/haku1806/Auto-Blood/master/config.txt
 # Powershell script to auto enable remote desktop for administrator
 sudo touch /floppy/EnableRDP.ps1
 sudo echo -e "Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name \"fDenyTSConnections\" -Value 0" >> /floppy/EnableRDP.ps1
@@ -232,7 +226,66 @@ fi
 #
 # Running the KVM
 echo "[ Running the KVM ]"
-#if [ $skipped = 0 ] ; then
+if [ $skipped = 0 ] ; then
 echo "[.] running QEMU-KVM"
 sudo $qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format -drive file=$custom_param_os,index=1,media=cdrom -drive file=$custom_param_sw,index=2,media=cdrom $other_drives -boot once=d -vnc :5 &	
+# [note- no sudo should be used after that]
+#pidqemu=$(pgrep qemu) # does not work
+pid=$(echo $! | head -1)
+disown -h $pid
+echo "disowned PID : "$pid
+echo "[ For Debugging purpose ]"
+echo -e "$qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format -drive file=$custom_param_os,index=1,media=cdrom -drive file=$custom_param_sw,index=2,media=cdrom $other_drives -boot once=d -vnc :5 & disown %1"
+if [ $mounted = 1 ]; then
+echo -e "wget -P /tmp https://archive.org/download/vkvm.tar_201903/vkvm.tar.gz && tar -C /tmp -zxvf /tmp/vkvm.tar.gz && rm /tmp/vkvm.tar.gz && $qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format $other_drives -boot c -vnc :5 & disown %1" > /details.txt
+else
+echo -e "$qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format $other_drives -boot c -vnc :5 & disown %1" > /details.txt
+fi
+echo -e "${YELLOW} SAVE BELOW GREEN COLORED COMMAND IN A SAFE LOCATION FOR FUTURE USAGE${NC}"
+if [ $mounted = 1 ]; then
+echo -e "${GREEN_D}wget -P /tmp https://archive.org/download/vkvm.tar_201903/vkvm.tar.gz && tar -C /tmp -zxvf /tmp/vkvm.tar.gz && /tmp/rm vkvm.tar.gz && $qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format $other_drives -boot c -vnc :5 & disown %1${NC}"
+else
+echo -e "${GREEN_D}$qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk$format $other_drives -boot c -vnc :5 & disown %1${NC}"
+fi
+echo -e "${BLUE}command also saved in /details.txt file${NC}"
+echo -e "${YELLOW}Now download 'VNC Viewer' App from here :${NC} https://www.realvnc.com/en/connect/download/viewer/\n${YELLOW}Then install it on your computer${NC}"
+echo -e "Finally open ${GREEN_D}$ip:9${NC} on your VNC viewer."
+if [ $mounted = 1 ]; then
+read -r -p "Had your Windows Server setup completed successfully? (yes/no) : " setup_initial
+setup_initial=$(echo "$setup_initial" | head -c 1)
+sleep 10
+if [ ! -z $setup_initial ] && [ $setup_initial = 'Y' -o $setup_initial = 'y' ] ; then
+echo $pid $cpus $custom_param_disk $custom_param_sw $other_drives
+echo "helper called" 
+for i in $(ps aux | grep -i "qemu" | head -2 | tr -s '[:space:]' | cut -f2 -d" ") ; do echo "killing process id : "$i ; kill -5 $i ; done
+#sleep 30
+echo "un-mounting"
+umount -l /mnt
+sleep 10
+df
+sync; echo 3 > /proc/sys/vm/drop_caches
+free -m 
+availableRAM=$(echo $availableRAMcommand | bash)
+custom_param_ram="-m "$(expr $availableRAM - 200 )"M"
+custom_param_ram2="-m "$(expr $availableRAM - 500 )"M"
+echo $custom_param_ram
+echo "[..] running QEMU-KVM again"
+$qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk -drive file=$custom_param_sw,index=1,media=cdrom $other_drives -boot c -vnc :5 &
+pid2=$(echo $! | head -1)
+disown -h $pid2
+echo "disowned PID : "$pid2
+echo "[ For Debugging purpose ]"
+echo -e "$qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk -drive file=$custom_param_sw,index=1,media=cdrom $other_drives -boot c -vnc :5 & disown %1"
+echo -e "${YELLOW} SAVE BELOW GREEN COLORED COMMAND IN A SAFE LOCATION FOR FUTURE USAGE${NC}"
+echo -e "${GREEN}wget -P /tmp https://archive.org/download/vkvm.tar_201903/vkvm.tar.gz && tar -C /tmp -zxvf /tmp/vkvm.tar.gz && rm /tmp/vkvm.tar.gz && $qemupath -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor $custom_param_ram2 -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=$custom_param_disk,index=0,media=disk $other_drives -boot c -vnc :5 & disown %1${NC}"
+echo -e "Now you can access your Windows server through \"VNC viewer\" or \"Remote Desktop Application\" (if your server 'Remote Desktop' is enabled)."
+echo "Job Done :)"
+fi
+else
+echo "Job Done :)"
+fi
+else
+echo "Windows OS required at least 25GB free desk space. Your Server/VPS does't have 25GB free space!"
+echo "Exiting....."
+fi
 fi
